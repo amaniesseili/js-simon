c7693a9c4eb98246932b0d9e121a89711cb5e3b6
amaniesseili <esseiliamani@hotmail.com>
2023-07-13 19:10:05 +0200

    recupero gli elementi html in js

---
+++ main.js
@@ -0,0 +1,42 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const startingMinutes = 10 ;   // 10 minuti
+let time = startingMinutes * 60 ; // 10x60= 600s
+
+const countdownElement = document.getElementById("countdown"); // recupero l'element dal html 
+
+setInterval(updateCountdown, 1000);  // set interval per chiamare questa funzione ogni secondo 
+
+function updateCountdown(){   //funzione per aggiornare il countdown
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60 ;
+
+   
+    countdownElement.innerHTML = `${minutes}:${seconds}`
+    time--;
+}*/
